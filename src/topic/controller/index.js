fbcb6ac969f11391801d2da5368dddc2c2cd9605
newbyebye <newbyebye@github.com>
2017-01-09 22:56:25 +0800

    fix index page

--- src/topic/controller/index.js
+++ src/topic/controller/index.js
@@ -33,58 +33,7 @@ export default class extends Base {
         return;
       }
       this.setCorsHeader();
-      let map = {
-        'pid':0,
-        'status': 1,
-      };
-      //排序
-      let o = {};
-      let order =this.get('order')||100;
-      //console.log(order);
-      order = Number(order);
-      switch (order){
-        case 1:
-          o.update_time = 'ASC';
-          break;
-        case 2:
-          o.view = 'DESC';
-          break;
-        case 3:
-          o.view = 'ASC';
-          break;
-        default:
-          o.update_time = 'DESC';
-      }
 
-      this.assign('order',order);
-      let data = await this.model('document').where(map).page(this.param('page'),10).order(o).countSelect();
-      this.assign("list",data);
-      //console.log(data);
-      if(this.isAjax("get")){
-        for(let v of data.data){
-          if(!think.isEmpty(v.pics)){
-            let arr = []
-            for (let i of v.pics.split(",")){
-              arr.push(await get_pic(i,1,300,169))
-            }
-            v.pics = arr;
-          }
-          if(!think.isEmpty(v.cover_id)){
-            v.cover_id = await get_pic(v.cover_id,1,300,169);
-          }
-          if(!think.isEmpty(v.price)){
-            if(!think.isEmpty(get_price_format(v.price,2))){
-              v.price2 = get_price_format(v.price,2);
-            }
-            v.price = get_price_format(v.price,1);
-
-          }
-          v.uid = await get_nickname(v.uid);
-          v.url = get_url(v.name,v.id);
-          v.update_time = moment(v.update_time).fromNow()
-        }
-        return this.json(data);
-      }
       return this.display(`mobile/${this.http.controller}/${this.http.action}`)
     }else{
       //debugger;
