205381d3003fee542d42355584897e3fff7005d6
newbyebye <newbyebye@github.com>
2017-01-22 06:56:45 +0800

    fix wechat redirect

--- src/topic/controller/base.js
+++ src/topic/controller/base.js
@@ -13,8 +13,7 @@ export default class extends think.controller.base {
   }
 
   async __before() {
-    // TODO: 微信重定向失败
-    //await this.action("uc/weixin", "oauth");
+    await this.action("uc/weixin", "oauth");
     //网站配置
     this.setup = await this.model("setup").getset();
     // console.log(this.setup);
