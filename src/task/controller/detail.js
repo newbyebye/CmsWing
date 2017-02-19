cda3207c38350d459673f0c1568f118e52fe98d2
newbyebye <newbyebye@github.com>
2017-02-19 17:30:08 +0800

    增加统计

--- src/task/controller/detail.js
+++ src/task/controller/detail.js
@@ -16,6 +16,8 @@ export default class extends Base {
       return think.statusAction(404, this.http);
     }
 
+    this.model('task_link').where({id:p}).increment("view_num", 1);
+
     let user = await this.model('wx_user').where({uid: taskLink.user_id}).find();
     this.assign('user', user);
 
@@ -23,6 +25,7 @@ export default class extends Base {
     let content = await this.model('document_article').where({id: taskLink.document_id}).find();
 
     doc.content = content.content.split("_ueditor_page_break_tag_");
+
 
     this.assign('info', doc); 
 
@@ -33,12 +36,15 @@ export default class extends Base {
     await this.weblogin();
     console.log(this.user, this.http.ip());
 
-    let taskLink = await this.model('task_link').where({id:p}).find();
+    let taskLink = await this.model('task_link').where({id:this.param("id")}).find();
     if (think.isEmpty(taskLink)){
       return this.json({"errno":404,"errmsg":"error"});
     }
-
+    
     let result = await this.model("task_record").thenAdd({task_link_id: this.param("id"), user_id: this.user.uid, action_ip:_ip2int(this.http.ip()), create_time: new Date().getTime()}, {task_link_id: this.param("id"), user_id: this.user.uid});
+    if (result.type == "add"){
+      this.model('task_link').where({id:this.param("id")}).increment("redirect_num", 1);
+    }
 
     return this.json({});
   }
